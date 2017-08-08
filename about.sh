200c7d703c20ca0e552fb8eda8ba9f133668c4ee
Iam <ilham.say36@yahoo.com>
2017-08-08 16:30:59 +0700

    Create about.sh

---
+++ about.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#About this script
+
+clear
+echo -e "Script Auto Install SSH & OpenVPN v1.0" 
+echo -e ""
+echo -e "For Debian 7 32 bit & 64 bit"
+echo -e "For VPS with KVM and VMWare virtualization"
+echo -e ""
+echo -e "Original script by :"
+echo -e "* Fornesia"
+echo -e "* Rzengineer"
+echo -e "* Fawzya"
+echo -e ""
+echo -e "Mod by Ilham Muhammad"
+echo -e ""
+echo -e "https://www.facebook.com/Ilham02muhammad"
+echo -e ""
